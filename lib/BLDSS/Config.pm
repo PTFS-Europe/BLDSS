2364f6952a362e63b9ea2aac57e31d24c9440d81
Colin Campbell <colin.campbell@ptfs-europe.com>
2015-01-23 16:26:31 +0000

    uAdd Dev account to Config

--- lib/BLDSS/Config.pm
+++ lib/BLDSS/Config.pm
@@ -2,12 +2,21 @@ package BLDSS::Config;
 use strict;
 use warnings;
 
-
 sub new {
     my $class = shift;
 
     my $self = {
-        customer_account_id => q{1151321}, #API Key
+        customer_account_id => q{1151321},    #API Key
+        api_application_id  => {
+            key  => 'BLAPluy8my',
+            auth => '3bo7y9vYjK1',
+            name => 'KohaTestILL',
+            id   => 25,
+        },
+
+        # BL default developer's test account
+        account      => '87-0656',
+        api_key_auth => 'APIDEV176',
     };
 
     bless $self, $class;
@@ -17,6 +26,11 @@ sub new {
 sub customer_account_id {
     my $self = shift;
     return $self->{customer_account_id};
+}
+
+sub api_application_id {
+    my $self = shift;
+    return $self->{api_application_id}->{id};
 }
 
 1;
